c4bfb0e4dcdb501e8669a282941d61648be3ae22
Mohd Aasim <masimshaikh8976@gmail.com>
2026-03-07 14:38:56 +0530

    made request structure, and data handling

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const http = require('http');
+const { json } = require('stream/consumers');
+let notes = [];
 const server = http.createServer((req,res)=>{
     if (req.url === "/" && req.method === "GET") {
         res.write("Welcome to Notes API");
         res.end();
     }
     else if(req.url === '/notes' && req.method ==='GET'){
-        res.write('here are all notes');
-        res.end();
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(notes));
     }
-    else if(req.url ==='/createnotes' && req.method === 'POST'){
-        res.write('create new note');
-        res.end();
+    else if(req.url ==='/notes' && req.method === 'POST'){
+        let body = "";
+        req.on("data", chunk => {
+            body += chunk;
+        });
+        req.on("end", () =>{
+            const newnote = JSON.parse(body);
+            notes.push(newnote);
+            res.writeHead(201,{"content-type":"application/json"});
+            res.end(JSON.stringify(newnote));
+
+        });
     }
     else{
         res.write('no route find');
