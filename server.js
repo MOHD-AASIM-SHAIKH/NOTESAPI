31a589d930c07b4cd5c97ed7c996193bb98ade82
Mohd Aasim <masimshaikh8976@gmail.com>
2026-03-07 15:24:00 +0530

    added id to notes for creating and fetcing

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const server = http.createServer((req,res)=>{
         res.write("Welcome to Notes API");
         res.end();
     }
-    else if(req.url === '/notes' && req.method ==='GET'){
+    else if(req.url ==='/notes' && req.method === "GET"){
         const data =  fs.readFileSync("notes.json");
         const notes  = JSON.parse(data);
-        res.writeHead(200, {"Content-Type": "application/json"});
+         res.writeHead(200, {"Content-Type": "application/json"});
         res.end(JSON.stringify(notes));
+    }
+    else if(req.url.startsWith('/notes/') && req.method ==='GET'){
+        const parts = req.url.split('/');
+        const id = parts[2];
+        const data =  fs.readFileSync("notes.json");
+        const notes  = JSON.parse(data);
+        const note = notes.find(n => n.id == id);
+        if(!note){
+            res.writeHead(404);
+            return res.end("not found");
+        }
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(note));
     }
     else if(req.url ==='/notes' && req.method === 'POST'){
         let body = "";
@@ -22,6 +35,7 @@ const server = http.createServer((req,res)=>{
             const newnote = JSON.parse(body);
             const data = fs.readFileSync("notes.json");
             const notes =  JSON.parse(data);
+            newnote.id = Date.now();
             notes.push(newnote);
             fs.writeFileSync("notes.json",JSON.stringify(notes));
             res.writeHead(201,{"content-type":"application/json"});
