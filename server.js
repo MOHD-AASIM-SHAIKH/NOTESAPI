16ea5acc430d8920de68764e235e42b63e9a5119
Mohd Aasim <masimshaikh8976@gmail.com>
2026-03-07 13:09:53 +0530

    made manual routing and http

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    res.write("hello this node server");
-    res.end();
+    if (req.url === "/" && req.method === "GET") {
+        res.write("Welcome to Notes API");
+        res.end();
+    }
+    else if(req.url === '/notes' && req.method ==='GET'){
+        res.write('here are all notes');
+        res.end();
+    }
+    else if(req.url ==='/createnotes' && req.method === 'POST'){
+        res.write('create new note');
+        res.end();
+    }
+    else{
+        res.write('no route find');
+        res.end();
+    }
 });
 server.listen(3000,() =>{
     console.log("server is running on port 3000");
