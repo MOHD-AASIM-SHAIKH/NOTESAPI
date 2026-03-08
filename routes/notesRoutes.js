5ab611e7b230bd5ee2d22e7dd995949d95bd6116
Mohd Aasim <mohdaasimsky0@gmail.com>
2026-03-08 10:52:00 +0530

    made routes

---
+++ routes/notesRoutes.js
@@ -0,0 +1,24 @@
+const controller = require("../controllers/notesController");
+
+function notesroutes(req,res){
+    if(req.url === '/notes' && req.method ==='GET'){
+        return controller.getnotes(req,res);
+    }
+    if(req.url ==='/notes' && req.method === 'POST'){
+        return controller.createnotes(req,res);
+    }
+    if(req.url.startwith('/notes')){
+        const id = req.url.split('/')[2];
+
+        if(req.method === 'GET'){
+            return controller.getnote(req,res,id);
+        }
+        if(req.method === 'DELETE'){
+            return controller.deletenote(req,res);
+        }
+        if(req.method === 'PUT'){
+            return controller.updatenote(req,res,id);
+        }
+    }
+}
+module.exports = notesroutes;
